71fa6c3a3e053739884d41b6c7b0337ec8db4f8f
StefanoMast <ste.mastrantonio@gmail.com>
2022-04-28 15:21:39 +0200

    add primo bonus

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Viene fornito un array di oggetti che rappresentano i membri del team.
+// Ogni membro ha le informazioni necessarie per stampare le relative informazioni: Nome, Ruolo e Foto.
+// MILESTONE 1:
+// stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// stampare le stesse informazioni su DOM sottoforma di stringhe
+
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+  let userInfo = "";
+  for (let i = 0; i < team.length; i++) {
+      userInfo = team[i];
+      console.log(userInfo.name, userInfo.role, userInfo.image);
+  
+      const print = document.getElementById("infoTeam");
+      print.innerHTML += `<img src="img/${userInfo.image}"> <br> ${"Nome: "} ${userInfo.name} <br> ${"Ruolo: "} ${userInfo.role} <br><hr>`;
+  }
